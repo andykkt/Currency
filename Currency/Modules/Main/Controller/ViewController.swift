2ce3a7029c1a3133f2cb295488e58ff2bf4f1d65
Andy Kim <andy.kim@optus.com.au>
2018-06-28 23:02:48 +1000

    Initial commit

---
+++ Currency/Modules/Main/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Currency
+//
+//  Created by Andy Kim on 28/6/18.
+//  Copyright © 2018 HoodlesApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
